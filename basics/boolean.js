44cc915f000b577f4f94b10f9dbf6a7437db9445
KaoriIzutani <41350890+KaoriIzutani@users.noreply.github.com>
2019-03-27 20:31:51 -0700

    Comparison Operators
    Comparison Operators

---
+++ basics/boolean.js
@@ -0,0 +1,20 @@
+//Comparison Operators
+// === means equality operator. Left and right var/values must be equal to be true
+// !== means if the var on the right is not the same as the left, bool returns true
+// <= means less than or equal to
+// >= greater than or equal to
+
+// let CurrentTemp = 32
+// let isFreezing = CurrentTemp <= 32
+// console.log(isFreezing)
+
+/* A real life challenge. Try programming for an amusement park, where discounts apply for 
+children under age 7 and seniors over age 65. Everybody else gets the standard pricing.*/
+
+let MyAge = 66
+
+let isChild = MyAge <= 7
+console.log(isChild)
+
+let isSenior = MyAge >= 65
+console.log(isSenior)
